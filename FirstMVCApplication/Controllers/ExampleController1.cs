f5aa73d593d24868e15e58745a68dd1e46fbfc2a
Ausha M S <anusgowda050@gmail.com>
2023-02-28 17:55:47 +0530

    Creating view.

---
+++ FirstMVCApplication/Controllers/ExampleController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class ExampleController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstMVCApplication/Models/Create.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FirstMVCApplication.Models
+{
+    public class Create
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
